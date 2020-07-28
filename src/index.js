76b96e57f1f825bbfc645b602564003be845bb56
leoalcantara <leonidio.ti@gmail.com>
2020-07-28 13:12:18 -0300

    installing react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './pages/cadastro/video';
+
+import  { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />  
+      <Route path="/cadastro/videos" component={CadastroVideo} exact />
+
+      <Route component={()=>(<h1>Erro 404</h1>)} />
+      {/* <Route component={App} /> */}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/cadastro/video/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CadastroVideo (){
+    return(
+      <div>
+        <h1>Cadastro de vídeo</h1>
+      </div>
+    );
+  }
+
+  export default CadastroVideo;
