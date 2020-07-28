153a7f8605f44389af32226ca26692aa4c19004b
leoalcantara <leonidio.ti@gmail.com>
2020-07-28 00:42:36 -0300

    inserting button in the menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../../assets/img/logo.png';
 
 import './styles.css';
+import ButtonLink from './components/ButtonLink';
 
 export default function Menu() {
     return (
@@ -10,6 +11,7 @@ export default function Menu() {
                 <a href="/">
                     <img className= "Logo" src={Logo} alt="Leoflix logo"/>
                 </a>
+                <ButtonLink className="ButtonLink" href="/"> Novo Video </ButtonLink>
             </nav>
             
         </div>
